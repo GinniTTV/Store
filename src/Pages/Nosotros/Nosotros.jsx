e3afb89de60ce7d2fc8bf542c9e0752ccff5e36d
Gino Cazzola <ginex.bluex@gmail.com>
2024-05-08 02:13:08 -0300

    sección nosotros, estilos y más

--- src/Pages/Nosotros/Nosotros.jsx
+++ src/Pages/Nosotros/Nosotros.jsx
@@ -4,22 +4,27 @@ import './Nosotros.scss';
 const Nosotros = () => {
   return (
     <div>
-      <h1>Nosotros</h1>
       <main className="main-container">
         <div className="nosotros-container">
           <div className="d-center">
-            <h2>¡Bienvenidos a Juguetería Cósmica!</h2>
+            <h2>¡Bienvenidos a Toyland!</h2>
             <h3>Donde los sueños se vuelven realidad</h3>
           </div>
+          <img src="/assets/Icons/Peluches.colours.png" alt="Banner" className="banner-img" />
           <div className="section-container align-center">
             <p>
-              En Juguetería Cósmica, creemos en la magia de la infancia. Nuestro objetivo es crear un mundo lleno de alegría, diversión y aprendizaje para niños y niñas de todas las edades. Cada juguete que ofrecemos está diseñado con amor y cuidado para inspirar la imaginación y fomentar el juego creativo.
+              En Toyland, creemos en la magia de la infancia. Nuestro objetivo es crear un mundo lleno de alegría, diversión y aprendizaje para niños y niñas de todas las edades. Cada juguete que ofrecemos está diseñado con amor y cuidado para inspirar la imaginación y fomentar el juego creativo.
             </p>
           </div>
           <div className="section-container align-center">
             <p>
-              Nos apasiona brindar experiencias únicas a nuestros clientes. Desde juguetes clásicos hasta las últimas novedades, nuestra colección es una celebración de la diversidad y la innovación en el mundo del juego. Estamos comprometidos a proporcionar la mejor calidad y el mejor servicio, para que cada experiencia con Juguetería Cósmica sea inolvidable.
+              Nos apasiona brindar experiencias únicas a nuestros clientes. Desde juguetes clásicos hasta las últimas novedades, nuestra colección es una celebración de la diversidad y la innovación en el mundo del juego. Estamos comprometidos a proporcionar la mejor calidad y el mejor servicio, para que cada experiencia con Toyland sea inolvidable.
             </p>
+          </div>
+          <img src="/assets/Icons/Estantería.png" alt="Estantería" className="estanteria-img" />
+          <div className="thanks-message">
+            <h4>¡Gracias por visitarnos!</h4>
+            <p>Esperamos que tu experiencia en Toyland sea llena de diversión y alegría. ¡Te esperamos pronto!</p>
           </div>
         </div>
       </main>
@@ -28,3 +33,4 @@ const Nosotros = () => {
 };
 
 export default Nosotros;
+
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -19,12 +19,12 @@ const NavBar = () => {
             <Link to="/" className='navbar-logo'>Tienda</Link>
             <ul className='nav-links'>
                 <li><Link to="/" className='nav-link'>Inicio</Link></li>
-                <li><Link to="/alta" className='nav-link'>Alta</Link></li>
+                <li><Link to="/alta" className='nav-link'>alta</Link></li>
                 <li><Link to="/contacto" className='nav-link'>contacto</Link></li>
                 <li><Link to="/nosotros" className='nav-link'>nosotros</Link></li>
             </ul>
             <button className='cart-link' onClick={openModal}>
-            <img src={process.env.PUBLIC_URL + '/assets/Icons/add-cart-wv.png'} alt="Carrito de compras" />
+            <img src={process.env.PUBLIC_URL + '/assets/Icons/add-cart.violet.png'} alt="Carrito de compras" />
 
             </button>
             {modalOpen && <CartModal closeModal={closeModal} />}
